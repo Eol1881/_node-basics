2d2a8de6c86a755968ae203b323414b1058c6d83
Eol1881 <02f1881@gmail.com>
2023-05-03 18:01:23 +0300

    feat: start

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-// const { stdout } = process; <==> stdout = process.stdout;
-// const { stdin, stdout } = process; <==> stdin = process.stdin; stdout = process.stdout;
-const { stdin, stdout } = process; // Деструктуризация объекта
-stdout.write(`Server started` + '\n');
-stdout.write(`Please enter your name: `);
+const { stdin, stdout } = require('process');
 
-let userName = '';
-
-stdin.on('data', (data) => { // data - это тип эвента,  on - подобие eventListener
-  userName = data.toString().trim();
-  stdout.write(`Hello, ${userName}. Nice to meet you!` + '\n');
-  stdout.write(`Your reversed name is: ${userName.split('').reverse().join('')}` + '\n');
+stdout.write('Hello');
+stdin.on('data', function (data) {
+  stdout.write(data);
   process.exit();
-});
-
-process.on('exit', () => stdout.write(`Goodbye, ${userName}!`));
-// ПОРЯДОК ОБЯВЛЕНИЯ ОБРАБОТЧИКОВ НЕ ВАЖЕН - ОНИ ИНИЦИАЛЗИРУЮТСЯ В САМОМ НАЧАЛЕ
+})
